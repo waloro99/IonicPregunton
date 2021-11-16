e4a1e1098380a21b7ec385031c053dda82627601
Walter Orozco <walterof1@hotmail.com>
2021-11-16 15:36:04 -0600

    tab 2 interfaz

--- preguntasApp/src/app/tab2/tab2.page.ts
+++ preguntasApp/src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSegment } from '@ionic/angular';
 import { PreguntasService } from '../services/preguntas.service';
 
 @Component({
@@ -8,11 +9,19 @@ import { PreguntasService } from '../services/preguntas.service';
 })
 export class Tab2Page implements OnInit{
 
+  @ViewChild(IonSegment, { static: true }) segment: IonSegment;
+
+  categorias = ['Sports', 'History', 'Animals', 'Vehicles', 'Celebrities', 'Video Games'];
+  form = ['1','2','3','4']
   constructor(private preguntasService: PreguntasService) {}
 
   ngOnInit(){
     this.preguntasService.getFeature()
     .subscribe(console.log);
+  }
+
+  cambioCategoria(event){
+    console.log('hacer algo');
   }
 
 }
