5f95923c0aad8cfc16adfa396664e01ce87fe04c
Walter Orozco <walterof1@hotmail.com>
2021-11-17 19:33:42 -0600

    preguntas mostradas por categoria

--- preguntasApp/src/app/services/preguntas.service.ts
+++ preguntasApp/src/app/services/preguntas.service.ts
@@ -7,10 +7,19 @@ import { ContenidoPreguntas } from '../interfaces/interfaces';
 })
 export class PreguntasService {
 
+  categoriaActual = 0;
+
   constructor( private http: HttpClient) { }
 
   getFeature(){
     return this.http.get<ContenidoPreguntas>(`https://opentdb.com/api.php?amount=30&difficulty=easy&type=multiple`);
+  }
+
+  getPreguntasCategoria(categoria: number){
+    if (this.categoriaActual !== categoria) {
+      this.categoriaActual = categoria;
+    }
+    return this.http.get<ContenidoPreguntas>(`https://opentdb.com/api.php?amount=20&category=${categoria}&difficulty=easy&type=multiple`);
   }
 
 }
--- preguntasApp/src/app/tab2/tab2.page.ts
+++ preguntasApp/src/app/tab2/tab2.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { IonSegment } from '@ionic/angular';
 import { PreguntasService } from '../services/preguntas.service';
+import { ContenidoPreguntas, Preguntas } from '../interfaces/interfaces';
 
 @Component({
   selector: 'app-tab2',
@@ -11,17 +12,43 @@ export class Tab2Page implements OnInit{
 
   @ViewChild(IonSegment, { static: true }) segment: IonSegment;
 
-  categorias = ['Sports', 'History', 'Animals', 'Vehicles', 'Celebrities', 'Video Games'];
-  form = ['1','2','3','4']
+  categorias = ['Sports', 'History', 'Books', 'Music', 'Television', 'Video Games'];
+  preguntas: Preguntas[] = [];
+  selectCategoria = '';
+  numberCate: number = 21;
   constructor(private preguntasService: PreguntasService) {}
 
   ngOnInit(){
-    this.preguntasService.getFeature()
-    .subscribe(console.log);
+   // this.preguntasService.getFeature()
+   // .subscribe(console.log);
   }
 
   cambioCategoria(event){
-    console.log('hacer algo');
+    this.preguntas = [];
+    this.selectCategoria = event.detail.value;
+    this.cargarNoticias(event.detail.value);
+    //console.log(event.detail.value);
+  }
+
+  cargarNoticias(categoria: string, event ?){
+    if(categoria === 'History'){
+      this.numberCate = 23;
+    }else if (categoria === 'Books') {
+      this.numberCate = 10;
+    }else if (categoria === 'Music') {
+      this.numberCate = 12;
+    }else if (categoria === 'Television') {
+      this.numberCate = 14;
+    }else if (categoria === 'Video Games') {
+      this.numberCate = 15;
+    }else{
+      this.numberCate = 21;
+    }
+    this.preguntasService.getPreguntasCategoria(this.numberCate)
+    .subscribe(resp => {
+      //console.log(resp.results);
+      this.preguntas.push(...resp.results);
+    });
   }
 
 }
