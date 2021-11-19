251919aa46e0deaa84124b2035cd7b7bab02934f
Walter Orozco <walterof1@hotmail.com>
2021-11-18 18:26:04 -0600

    ordena completo final proyecto

--- preguntasApp/src/app/tab3/tab3.page.ts
+++ preguntasApp/src/app/tab3/tab3.page.ts
@@ -15,8 +15,10 @@ export class Tab3Page {
     
   }
 
+  //ordena por respuesta, tiempo min y tiempo seg
   async ionViewWillEnter(){
     this.personas = await this.psCtrl.obtenerUsuarios();
+    this.personas.sort(((a,b) => b.respuestas - a.respuestas || a.tiempoM - b.tiempoM || a.tiempoS - b.tiempoS));
   }
 
 }
